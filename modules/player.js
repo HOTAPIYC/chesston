27c00fb2f1a07f48e7525409123a91f86fe08b34
HOTAPIYC <andreas_postkasten@gmx.net>
2020-05-02 20:48:55 +0200

    Added support for promotion and castling

--- modules/player.js
+++ modules/player.js
@@ -45,14 +45,30 @@ class HumanPlayer {
     selectPiece(event) {
         if(event.target.className.search(new RegExp(`fen-.-${this.color}`)) > -1) {
             this.possibleMoves = new Map(); // Reset possible moves
-            this.possibleMoves.set(event.target.id, 'root');
+            this.possibleMoves.set(event.target.id, {move:'root', promotion: -1});
 
             const moves = this.board.getPossibleMoves(event.target.id);
 
             moves.forEach(move => {
                 // Decode targets from moves in SAN notation
-                const targetSquare = /[a-h][1-8]/
-                this.possibleMoves.set(move.match(targetSquare)[0], move);
+                let re1 = new RegExp(/O-O/);
+                const castlingShort = re1.test(move);
+                let re2 = new RegExp(/O-O-O/);
+                const castlingLong = re2.test(move);
+                let re3 = new RegExp(/=[BNRQbnrq]/);
+                const promotion = re3.test(move);
+
+                let key = ''
+
+                if(!castlingLong && !castlingShort){
+                    key = move.match(/[a-h][1-8]/)[0];
+                } else if(castlingLong){
+                    key = this.color === 'w' ? 'c1' : 'c8';
+                } else if(castlingShort){
+                    key = this.color === 'w' ? 'g1' : 'g8';
+                }
+
+                this.possibleMoves.set(key, {move: move, promotion: promotion});
             });
             this.board.setHighlight(this.possibleMoves);
             return true;
@@ -62,14 +78,27 @@ class HumanPlayer {
         }
     }
 
-    selectTarget(event) {
+    async selectTarget(event) {
         if(event.target.classList.contains('highlight')) {
             const selectedMove = this.possibleMoves.get(event.target.id);
-            this.board.makeMove(selectedMove);
-            return false;
+            if(!selectedMove.promotion){
+                this.makeMove(this.board, selectedMove.move);
+                return false;
+            } else {
+                const dialog = new DialogPromotion();
+                const result = await dialog.show();
+
+                this.board.makeMove(selectedMove.move.replace(/=[BNRQbnrq]/, `=${result}`));
+
+                return false;
+            }
         } else {
             return this.selectPiece(event);
         }
+    }
+
+    makeMove(board, selectedMove){
+        board.makeMove(selectedMove);
     }
 }
 
--- modules/utils.js
+++ modules/utils.js
@@ -19,3 +19,44 @@ class EventObserver{
         });
     }
 }
+
+class Dialog{
+    constructor(){
+        this.id = '';
+    }
+
+    show(){
+        document.querySelector(`#${this.id}`).style.display = 'block';
+        return new Promise((resolve,reject) => {
+            document.querySelector(`#${this.id}-close`).addEventListener('click', event => {
+                document.querySelector(`#${this.id}`).style.display = 'none';
+                resolve(this.getResult());
+            });
+        });
+    }
+
+    getResult(){
+        return '';
+    }
+}
+
+class DialogPromotion extends Dialog{
+    constructor(){
+        super();
+
+        this.id = 'promotion-dialog';
+        document.querySelector('#promo-Q').checked = true;
+    }
+
+    getResult(){
+        if(document.querySelector('#promo-Q').checked){
+            return document.querySelector('#promo-Q').value;
+        } else if(document.querySelector('#promo-R').checked){
+            return document.querySelector('#promo-R').value;
+        } else if(document.querySelector('#promo-B').checked){
+            return document.querySelector('#promo-B').value;
+        } else if(document.querySelector('#promo-N').checked){
+            return document.querySelector('#promo-N').value;
+        }
+    }
+}
