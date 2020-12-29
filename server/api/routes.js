dc765b5004b415ff4cc76b7f5b715f1dcec3ae1a
HOTAPIYC <andreas_postkasten@gmx.net>
2020-12-29 18:04:42 +0100

    Separated the games business logic into module

--- server/api/routes.js
+++ server/api/routes.js
@@ -1,86 +1,55 @@
 import express from "express";
-import { createPlayer, getPlayer, getAllPlayers } from "../services/player.js";
+import * as playerController from "../services/player.js";
+import * as gameController from "../services/games.js";
 
 const router = express();
 
-let requests = [];
-let games = [];
-
 router.get("/player", (req,res) => {
-  res.json(getAllPlayers())
+  res.json(playerController.getAllPlayers())
 });
 
 router.post("/player/new", (req,res) => {
-  createPlayer(req.body.name, req.body.password)
+  playerController.createPlayer(req.body.name, req.body.password)
   .then((result) => res.status(200).json(result))
   .catch((error) => res.status(400).json(error));
 });
 
 router.post("/player/login", (req,res) => {
-  getPlayer(req.body.name, req.body.password)
+  playerController.getPlayer(req.body.name, req.body.password)
   .then((result) => res.status(200).json(result))
   .catch((error) => res.status(400).json(error));
 });
 
 router.get("/requests/:id", (req,res) => {
-  const filtered = requests.filter(request => {
-    if(request.to.id === req.params.id){
-      return request;
-    }
-  });
-  res.json(filtered);
+  res.json(gameController.getRequestsByPlayerId(req.params.id));
 });
 
 router.post("/requests", (req,res) => {
-  const request = {
-    id: uuidv4(),
-    from: req.body.from,
-    to: req.body.to
-  };
-
-  requests.push(request);
+  gameController.createRequest(req.body.from, req.body.to);
   res.json({ msg: "Request placed"});  
 });
 
 router.put('/requests/decline', (req,res) => {
-  requests = requests.filter(request => request.id !== req.body.id);
+  gameController.declineRequest(req.body.id);
   res.json({ msg: "Request removed"});
 });
 
 router.put("/requests/accept", (req,res) => {
-  const request = requests.find(request => request.id === req.body.id);
-  const game = {
-    id: uuidv4(),
-    white: request.to,
-    black: request.from,
-    fen: "",
-    lastMove: ""
-  };
-
-  games.push(game);
-
-  requests = requests.filter(request => request.id !== req.body.id);
+  gameController.acceptRequest(req.body.id);
   res.json({ msg: "Game created"});
 });
 
 router.get("/games/:id", (req,res) => {
-  const filtered = games.filter(game => {
-    if(game.white.id === req.params.id || game.black.id === req.params.id){
-      return game;
-    }
-  });
-  res.json(filtered);
+  res.json(gameController.getGameByPlayerId(req.params.id));
 });
 
 router.put("/games/:id/newmove", (req,res) => {
-  const index = games.indexOf(games.find(game => game.id === req.params.id));
-  games[index].lastMove = req.body.move;
+  gameController.addMoveToGame(req.params.id, req.body.move);
   res.json({ msg: 'Move placed' });
 });
 
 router.get("/games/:id/lastmove/:color", (req,res) => {
-  const game = games.find(game => game.id === req.params.id);
-  res.json({ lastMove: game.lastMove });
+  res.json(gameController.getLastMoveByPlayerId(req.params.id));
 });
 
 export { router }
---
+++ server/services/games.js
@@ -0,0 +1,61 @@
+import { v4 as uuidv4 } from "uuid";
+
+let requests = [];
+let games = [];
+
+function createRequest(from, to) {
+  const request = {
+    id: uuidv4(),
+    from: from,
+    to: to
+  };
+
+  requests.push(request);
+}
+
+function declineRequest(id) {
+  requests = requests.filter(request => request.id !== id);
+}
+
+function acceptRequest(id) {
+  const request = requests.find(request => request.id === id);
+  const game = {
+    id: uuidv4(),
+    white: request.to,
+    black: request.from,
+    fen: "",
+    lastMove: ""
+  };
+
+  games.push(game);
+
+  requests = requests.filter(request => request.id !== id);
+}
+
+function getRequestsByPlayerId(id) {
+  return requests.filter(request => {
+    if(request.to.id === id){
+      return request;
+    }
+  });
+}
+
+function getGameByPlayerId(id) {
+  return games.filter(game => {
+    if(game.white.id === id || game.black.id === id){
+      return game;
+    }
+  });
+}
+
+function addMoveToGame(id, move) {
+  const index = games.indexOf(games.find(game => game.id === id));
+  games[index].lastMove = move;
+}
+
+function getLastMoveByPlayerId(id) {
+  const game = games.find(game => game.id === id);
+  return { lastMove: game.lastMove };
+}
+
+export { createRequest, declineRequest, acceptRequest, getRequestsByPlayerId, getGameByPlayerId, addMoveToGame, getLastMoveByPlayerId };
